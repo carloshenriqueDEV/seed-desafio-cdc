c0153497606045334dbc4ecfab519cde47fbb104
carlosDev <carloshenriquexvii@gmail.com>
2025-12-22 18:27:03 -0300

    feat: Inclusão das validações na classe de domínio.

---
+++ application/CadastroAutor/Autor.cs
@@ -0,0 +1,11 @@
+﻿namespace application.CadastroAutor
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Descricao { get; set; }
+        public DateTime RegistradoEm { get; set; }
+    }
+}
